9e482504b12e0975d2fb95ce109db10dcce36b3f
SharfarazMahmood <sharfarazmahmood@gmail.com>
2021-09-28 13:07:21 +0600

    Login & Signup verification, error handling added.

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'package:shop_app/providers/auth.dart';
+import 'package:shop_app/providers/auth_provider.dart';
 import 'package:shop_app/providers/orders_provider.dart';
 import 'package:shop_app/screens/auth_screen.dart';
 import 'package:shop_app/screens/edit_product_screen.dart';
--- lib/screens/auth_screen.dart
+++ lib/screens/auth_screen.dart
@@ -2,8 +2,9 @@ import 'dart:math';
 
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'package:shop_app/models/http_exception.dart';
-import 'package:shop_app/providers/auth.dart';
+
+import '../models/http_exception.dart';
+import '../providers/auth_provider.dart';
 
 enum AuthMode {
   Signup,
@@ -124,55 +125,6 @@ class _AuthCardState extends State<AuthCard> {
     );
   }
 
-
-  // Future<void> _submit() async {
-  //   if (!_formKey.currentState.validate()) {
-  //     // Invalid!
-  //     return;
-  //   }
-  //   _formKey.currentState.save();
-  //   setState(() {
-  //     _isLoading = true;
-  //   });
-  //   try {
-  //     if (_authMode == AuthMode.Login) {
-  //       // Log user in
-  //       await Provider.of<Auth>(context, listen: false).login(
-  //         _authData["email"],
-  //         _authData["password"],
-  //       );
-  //     } else {
-  //       // Sign user up
-  //       await Provider.of<Auth>(context, listen: false).signup(
-  //         _authData["email"],
-  //         _authData["password"],
-  //       );
-  //     }
-  //   } on HttpException catch (error) {
-  //     var errorMessage = "Authentication failed";
-  //     if (error.toString().contains('EMAIL_EXISTS')) {
-  //       errorMessage = 'Email is already in use';
-  //     } else if (error.toString().contains('INVALID_EMAIL')) {
-  //       errorMessage = 'Thid is not a valid email address';
-  //     } else if (error.toString().contains('WEAK_PASSWORD')) {
-  //       errorMessage = 'This password is too weak';
-  //     } else if (error.toString().contains('EMAIL_NOT_FOUND')) {
-  //       errorMessage = 'Could not find user with that email';
-  //     } else if (error.toString().contains('INVALID_PASSWORD')) {
-  //       errorMessage = 'Invalid password';
-  //     }
-  //     _showErrorDialog(errorMessage);
-  //   } catch (error) {
-  //     const errorMessage = "Could not authenticate you. Please try again later";
-  //     _showErrorDialog(errorMessage);
-  //   }
-    
-  //   setState(() {
-  //     _isLoading = false;
-  //   });
-  // }
-
-
   Future<void> _submit() async {
     if (!_formKey.currentState.validate()) {
       // Invalid!
@@ -211,8 +163,7 @@ class _AuthCardState extends State<AuthCard> {
       }
       _showErrorDialog(errorMessage);
     } catch (error) {
-      const errorMessage =
-          'Could not authenticate you. Please try again later.';
+      var errorMessage = 'Could not authenticate you. Please try again later.';
       _showErrorDialog(errorMessage);
     }
 
@@ -220,8 +171,6 @@ class _AuthCardState extends State<AuthCard> {
       _isLoading = false;
     });
   }
-
-
 
   void _switchAuthMode() {
     if (_authMode == AuthMode.Login) {
@@ -275,7 +224,7 @@ class _AuthCardState extends State<AuthCard> {
                     if (value.isEmpty || value.length < 5) {
                       return 'Password is too short!';
                     }
-                    // return null;
+                    return null;
                   },
                   onSaved: (value) {
                     _authData['password'] = value;
@@ -291,7 +240,7 @@ class _AuthCardState extends State<AuthCard> {
                             if (value != _passwordController.text) {
                               return 'Passwords do not match!';
                             }
-                            // return null;
+                            return null;
                           }
                         : null,
                   ),
